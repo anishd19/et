b5e8c35b2373875c0fbe006421bf2ff9dbb99787
Anish Dhamodaran <anishd19@gmail.com>
2018-05-08 12:16:31 +0530

    WIP commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
-import { Login } from './Login'
+import { Provider } from 'react-redux';
+import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
+import { Login } from './components/Login'
 import './App.css';
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route path="/login" component={Login}/>
-          {/* <Route path="/register" component={Register}/> */}
-          <PrivateRoute exact path="/" component={Home}/>
-          <PrivateRoute path="/dashboard" component={Dashboard}/>
-        </div>
-      </Router>
-    );
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route path="/login" component={Login}/>
+            {/* <Route path="/register" component={Register}/> */}
+            <PrivateRoute exact path="/" component={Home}/>
+            <PrivateRoute path="/dashboard" component={Dashboard}/>
+          </div>
+        </Router>
+      </Provider>
+      );
   }
 }
 
@@ -26,35 +46,5 @@ const Home = () => {
 const Dashboard = () => {
   return <div> Dashboard </div>
 }
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      fakeAuth.isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { from: props.location }
-          }}
-        />
-      )
-    }
-  />
-);
 
 export default App;
