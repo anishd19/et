0168c0fb24b0e50f70f32ed0dd2ad78432accb91
Anish Dhamodaran <anishd19@gmail.com>
2018-07-23 21:48:00 +0530

    adds logout functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 import './Navbar.css';
 
-export const Navbar = () => {
-  return (
-    <div className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="#">
-            <div className="logo">Expense Tracker</div>
-          </a>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            <a className="navbar-item">
-              Dashboard
+class Navbar extends Component {
+
+  onLogoutClick = () => {
+    this.props.logoutUser();
+  }
+  render() {
+    return (
+      <div className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="#">
+              <div className="logo">Expense Tracker</div>
             </a>
-            <a className="navbar-item">
-              Log out
-            </a>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <a className="navbar-item">
+                Dashboard
+              </a>
+              <a className="navbar-item" onClick={this.onLogoutClick}>
+                Log out
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+const connectedNavbar = connect(mapStateToProps, { logoutUser })(Navbar);
+export { connectedNavbar as Navbar };
