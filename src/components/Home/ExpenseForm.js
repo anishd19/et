8311761e0ef9da79b3a1a4bf1e6773921a17136a
Anish Dhamodaran <anishd19@gmail.com>
2018-08-12 19:09:54 +0530

    Changes expense form to a modal

--- src/components/Home/ExpenseForm.js
+++ src/components/Home/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { addExpense } from '../../actions/expenseActions';
 import './ExpenseForm.css';
@@ -10,11 +10,14 @@ class ExpenseForm extends Component {
       title: '',
       amount: '',
       categories: [],
-      errors: {}
+      errors: {},
+      showModal: false
     }
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
@@ -38,49 +41,73 @@ class ExpenseForm extends Component {
 
     this.props.addExpense(newExpense);
     this.setState({ title: '', amount: '' });
+    this.closeModal();
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  openModal() {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false
+    })
+  }
+
   render() {
-    const { errors } = this.state;
+    const { errors, showModal } = this.state;
 
     return (
-      <div className="container" id="expense-form">
-        <div className="field is-horizontal">
-          <div className="field-body">
-            <div className="field">
-              <input 
-                className="input" 
-                type="text" 
-                name="title" 
-                placeholder="title" 
-                value={this.state.title}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="field">
-              <input 
-                className="input" 
-                type="number" 
-                name="amount" 
-                placeholder="amount" 
-                value={this.state.amount}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="field">
-              <p className="control">
-                <button className="button is-success" onClick={this.onSubmit}>
-                  Submit
-                </button>
-              </p>
+      <Fragment>
+        <button className="button is-success is-fullwidth" onClick={this.openModal}>
+          Add Expense
+        </button>
+        <div className={"modal " + (showModal ? "is-active" : null)}>
+          <div class="modal-background" onClick={this.closeModal}></div>
+          <div className="modal-content">
+            <div className="box" id="expense-form">
+              <div className="field is-horizontal">
+                <div className="field-body">
+                  <div className="field">
+                    <input
+                      className="input"
+                      type="text"
+                      name="title"
+                      placeholder="title"
+                      value={this.state.title}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="field">
+                    <input
+                      className="input"
+                      type="number"
+                      name="amount"
+                      placeholder="amount"
+                      value={this.state.amount}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="field">
+                    <p className="control">
+                      <button className="button is-success" onClick={this.onSubmit}>
+                        Submit
+                      </button>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+          <button className="modal-close is-large" aria-label="close" onClick={this.closeModal}></button>
         </div>
-      </div>
+      </Fragment>
     )
   }
 }
